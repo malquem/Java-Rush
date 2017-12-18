a9f3662b0e02fdb939115e992280a0c9a5b77725
malquem <mk5522@ya.ru>
2017-12-18 16:45:44 +0300

    2 obj

---
+++ src/task02task0208.java
@@ -0,0 +1,14 @@
+/*
+Одного кота нам мало
+*/
+public class task02task0208 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat boris = new Cat();
+        Cat vasya = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
